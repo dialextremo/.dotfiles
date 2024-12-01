64417642ab4e5ae3ce782c6b9316486f3865e0c5
Juan Pablo Correa <54782677+dialextremo@users.noreply.github.com>
2024-12-01 07:35:28 -0500

    change modes for keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 --
 --Better navigation
-vim.keymap.set("n", "H", "^", { desc = "Move cursor to beginning of line" })
-vim.keymap.set("n", "L", "$", { desc = "Move cursor to end of line" })
-vim.keymap.set("n", "J", "}", { desc = "Move cursor to next space" })
-vim.keymap.set("n", "K", "{", { desc = "Move cursor to previous space" })
+vim.keymap.set({ "n", "v" }, "H", "^", { desc = "Move cursor to beginning of line" })
+vim.keymap.set({ "n", "v" }, "L", "$", { desc = "Move cursor to end of line" })
+vim.keymap.set({ "n", "v" }, "J", "}", { desc = "Move cursor to next space" })
+vim.keymap.set({ "n", "v" }, "K", "{", { desc = "Move cursor to previous space" })
